9e1643d066ab7dd8af2c8608b345e3e52e811fcb
haidarhamda <haidarhamda23@gmail.com>
2024-10-07 23:41:05 +0700

    hist equalization

---
+++ src/histEqualization.m
@@ -0,0 +1,25 @@
+function histogramEqualizaation(img)
+    [n, m, ~] = size(img);
+    hist=imhist(img)/(n*m);
+    histEq = zeros(n,m);
+
+    sum=0.0;
+    for i=1:256
+        sum=0.0;
+        for j=1:i
+            sum=sum+hist(j);
+        end
+        histEq(i)=floor(256*sum);
+    end
+
+    for i=1:n
+        for j=1:m
+            img(i,j)=histEq(img(i,j)+1);
+        end
+    end
+    imshow(img);
+end
+
+%i = imread("images\boat.bmp");
+%histogramEqualizaation(i);
+%histeq(i)
