0b7f17fa8e495aff892b188caf3a45415f0efa94
Ammar Rasyad Chaeroel <ammarasyad@gmail.com>
2024-10-08 19:34:41 +0700

    Return double instead of uint32 for histogram

--- src/customHistogram.m
+++ src/customHistogram.m
@@ -1,5 +1,5 @@
 function h = customHistogram(image)
-    h = zeros(256, 1, 'uint32');
+    h = zeros(256, 1);
     vectorized_image = numel(image);
     for i = 1 : vectorized_image
         X = bitand(image(i), 0xFF);
