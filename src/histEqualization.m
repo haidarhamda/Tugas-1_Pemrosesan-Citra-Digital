6f3f7c17ed2f2d0b169dda97ef38dc4cfbf7aeba
haidarhamda <haidarhamda23@gmail.com>
2024-10-08 20:18:34 +0700

    Update histEqualization.m

--- src/histEqualization.m
+++ src/histEqualization.m
@@ -1,25 +1,29 @@
-function [ret] = histEqualization(img)
-    [n, m, ~] = size(img);
-    hist=customHistogram(img)/(n*m);
-    histEq = zeros(256);
-
-    sum=0.0;
-    for i=1:256
+function [ret] = histEquaalization(img)
+    [n, m, c] = size(img);
+    
+    for channel=1:c
+        imgChannel=img(:,:,channel);
+        hist=customHistogram(imgChannel)/(n*m);
+        histEq = zeros(256,1);
         sum=0.0;
-        for j=1:i
-            sum=sum+hist(j);
+        for i=1:256
+            sum=0.0;
+            for j=1:i-1
+                sum=sum+hist(j);
+            end
+            histEq(i)=floor(256*sum);
         end
-        histEq(i)=floor(256*sum);
-    end
 
-    for i=1:n
-        for j=1:m
-            img(i,j)=histEq(img(i,j)+1);
+        for i=1:n
+            for j=1:m
+                imgChannel(i,j)=histEq(imgChannel(i,j)+1);
+            end
         end
+        
+        ret(:,:,channel) = imgChannel;
     end
-    ret = img;
 end
 
-%i = imread("images\boat.bmp");
-%histogramEqualizaation(i);
-%histeq(i)
+%i = imread("images\Lena512warna.bmp");
+%imshow(histEquaaalization(i));
+%imshow(histeq(i))
