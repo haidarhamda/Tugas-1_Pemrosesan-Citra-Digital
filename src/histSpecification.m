34d494f9f68df89f68d53b40602f00eccc20c96c
haidarhamda <haidarhamda23@gmail.com>
2024-10-08 20:52:25 +0700

    Update histSpecification.m

--- src/histSpecification.m
+++ src/histSpecification.m
@@ -1,52 +1,57 @@
-function [resutImg,resultHist]=histSpecification(imgTarget, imgRef)
-    [n, m, ~] = size(imgTarget);
-    [nref,mref,~] = size(imgRef);
-    if(n~=nref || m~=mref)
+function [resultImg,resultHist]=histSpecification(imgTarget, imgRef)
+    [n, m, c] = size(imgTarget);
+    [nref,mref,cref] = size(imgRef);
+    if(n~=nref || m~=mref || c ~= cref)
         imgRef = imresize(imgRef,[n,m]);
     end
-    dim=n*m;
-    refHist=customHistogram(imgRef)/(dim);
-    targetHist=customHistogram(imgTarget)/(dim);
-    eqHist = zeros(256);
-    refEqHist = zeros(256);
-    invHist = zeros(256);
-    for i=1:256
-        sum=0.0;
-        for j=1:i
-            sum=sum+targetHist(j);
-        end
-        eqHist(i)=floor(256*sum);
-    end
 
-    for i=1:256
-        sum=0.0;
-        for j=1:i
-            sum=sum+refHist(j);
+    for channel=1:c
+        imgTargetChannel = imgTarget(:,:,channel);
+        imgRefChannel = imgRef(:,:,channel);
+        dim=n*m;
+        refHist=customHistogram(imgRefChannel)/(dim);
+        targetHist=customHistogram(imgTargetChannel)/(dim);
+        eqHist = zeros(256);
+        refEqHist = zeros(256);
+        invHist = zeros(256);
+        for i=1:256
+            sum=0.0;
+            for j=1:i
+                sum=sum+targetHist(j);
+            end
+            eqHist(i)=floor(256*sum);
         end
-        refEqHist(i)=floor(256*sum);
-    end
-
-    for i=1:n
-        minval=abs(eqHist(i)-refEqHist(1));
-        minj=0;
-        for j=1:256
-            val=abs(eqHist(i)-refEqHist(j));
-            if(val<minval)
-                minval=val;
-                minj=j;
+    
+        for i=1:256
+            sum=0.0;
+            for j=1:i
+                sum=sum+refHist(j);
+            end
+            refEqHist(i)=floor(256*sum);
+        end
+    
+        for i=1:n
+            minval=abs(eqHist(i)-refEqHist(1));
+            minj=0;
+            for j=1:256
+                val=abs(eqHist(i)-refEqHist(j));
+                if(val<minval)
+                    minval=val;
+                    minj=j;
+                end
+            end
+            invHist(i)=minj;
+        end
+    
+        for i=1:n
+            for j=1:m
+                imgTargetChannel(i,j)=invHist(imgTargetChannel(i,j)+1);
             end
         end
-        invHist(i)=minj;
+        %imshow(imgTarget);
+        resultImg(:,:,channel) = imgTargetChannel;
+        resultHist = customHistogram(imgTargetChannel);
     end
-
-    for i=1:n
-        for j=1:m
-            imgTarget(i,j)=invHist(imgTarget(i,j)+1);
-        end
-    end
-    %imshow(imgTarget);
-    resutImg=imgTarget;
-    resultHist=customHistogram(imgTarget);
 end
 
 %target = imread("images\boat.bmp");
